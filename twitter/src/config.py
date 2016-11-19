602303700df9c04f5fa450baab163df0ede2174f
Lior <bialik@halo-analytics.com>
2016-11-19 21:51:38 +0200

    changed it  to download all tweets at a single run and ability to filter by dates.
    Right now it download all the tweets and then filter them before saving to CSV.

--- twitter/src/config.py
+++ twitter/src/config.py
@@ -6,6 +6,9 @@
 import argparse
 import datetime
 
+epochTime = datetime.datetime.strptime('0', '%S')
+currentTime = datetime.datetime.now()
+
 options = None
 
 def assertOptionsInit():
@@ -15,14 +18,14 @@ def assertOptionsInit():
 def getCmdLineOptions():
     parser = argparse.ArgumentParser(description='Twitter Statuses Fetcher')
 
-    parser.add_argument('-n', '--screenName', required=True, action='append',
+    parser.add_argument('-u', '--screenName', required=True, action='append',
                         help='The screen name of the person you want to fetch. Use -n <NAME1> -n <NAME2> for multiple names')
-    parser.add_argument('-t', '--tweetsToFetch', default=None, type=assertValidTweetsToFetch,
-                        help='The amount of twits you want to fetch. Can use \'all\'. If left empty the default is 50')
-    parser.add_argument('-s', '--startDate', default='user_creation_time', type=assertValidDate,
-                        help='The start date of all twits, use format y-m-d')
-    parser.add_argument('-e', '--endDate', default='current_time', type=assertValidDate,
-                        help='The end date of all twits, use format y-m-d')
+
+    parser.add_argument('-s', '--startDate', default=epochTime, type=assertValidDate,
+                        help='Filter the start date of all twits, use format yy-mm-dd')
+
+    parser.add_argument('-e', '--endDate', default=currentTime, type=assertValidDate,
+                        help='Filter the end date of all twits, use format yy-mm-dd')
 
     cmdLineOptions = parser.parse_args()
     
@@ -33,28 +36,20 @@ def getScreenName():
     assertOptionsInit()
     return options.screenName
 
-def getTweetsToFetch():
+
+def getStartDateToFilter():
     assertOptionsInit()
-    return options.tweetsToFetch
+    return options.startDate
 
-def getDatesToFetch():
+
+def getEndDateToFilter():
     assertOptionsInit()
-    return options.startDate, options.endDate
+    return options.endDate
 
-
-def assertValidTweetsToFetch(tweetsToFetch):
-    if tweetsToFetch == "all" or tweetsToFetch ==None:
-        return tweetsToFetch
-    else:
-        try:
-            int(tweetsToFetch)
-            return tweetsToFetch
-        except ValueError:
-            raise ValueError("Wrong input. tweetsToFetchcan be either an integer or \"all\"")
 
 def assertValidDate(date):
-    if date == 'user_creation_time' or date == 'current_time':
-        return None
+    if date is None:
+        return date
     else:
         try:
             date = datetime.datetime.strptime(date, '%y-%m-%d')
--- twitter/src/statusFetch.py
+++ twitter/src/statusFetch.py
@@ -1,5 +1,9 @@
 """
 @date:   November 17, 2016
+
+This program will get all the tweets made by a given user or users (maxing at 3218).
+user can filter
+
 @author: Lior Bialik & Avihai Ezaguy
 """
 
@@ -7,6 +11,8 @@ import twitter # https://github.com/bear/python-twitter
 import config
 import sys
 import datetime
+import csv
+import os
 
 # keys from https://apps.twitter.com/ for the app TUA_EE_Project_statusFetch:
 twitterConsumerKey = 'kPIBTAAB1n9Wp5MEu1fdwzsXj'
@@ -14,14 +20,18 @@ twitterConsumerSecret = 'dFj6lz5iLtUvXXacQLWuJ3xubMelP2WQep5efvm195TEMoo4JW'
 twitterAccessTokenKey = '799287561332162560-UFz71F1VPcNDns1fWV9qAj4qKRxkyxU'
 twitterAccessTokenSecret = '9KebOrRMw63HHZK9l31SsM5BnYJpz9nqyq1905zpYUCCQ'
 
-def getTimeStamp(api_TimeStamp):
-    timestamp = datetime.datetime.strptime(api_TimeStamp, '%a %b %d %H:%M:%S +0000 %Y')
+# Magics:
+MAX_NUMBER_OF_TWEETS_PER_REQUEST = 200
+
+
+def getTimestamp(apiTimeStamp):
+    timestamp = datetime.datetime.strptime(apiTimeStamp, '%a %b %d %H:%M:%S +0000 %Y')
     return timestamp
 
-def strTimeStamp(timestamp):
+
+def stringTimestamp(timestamp):
     fmt = '%Y-%m-%d %H:%M:%S: '
     return timestamp.strftime(fmt)
-    
 
 
 def getTwitterApi():
@@ -32,34 +42,36 @@ def getTwitterApi():
     return api
 
 
-def fetchStatusText(screenNames, tweetsToFetch, dates):
+def saveTweetsToFile(screenName, readableFormatTweetsList):
+    outputFileName = '%s_tweets.csv' % screenName
+    outputFilePath = os.path.join(os.getcwd(), outputFileName)
+    with open(outputFileName, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "created_at", "text"])
+        writer.writerows(readableFormatTweetsList)
+    print("%s tweets were saved at %s" % (screenName, outputFilePath))
+
+
+def fetchStatusText(screenNames, startDateFilter, endDateFilter):
     api = getTwitterApi()
     
     for screenName in screenNames:
-        if tweetsToFetch != None: # -t was selected
-    # TODO: need to create an external function for getting the statuses, and in case of all implement 'def getAllTweets'
-            statuses = api.GetUserTimeline(screen_name=screenName, count=tweetsToFetch)
-            completeStatusesText = [strTimeStamp(getTimeStamp(status.created_at)) + status.text for status in statuses]
-            for singleStatusText in completeStatusesText: # TODO: add separate printing function
-                print(singleStatusText)
-        
-        # -e, -s selected
-        else:
-            startDate = dates[0]
-            endDate = dates[1]
-            if endDate == None:
-                endDate = datetime.datetime.now()
-            if startDate == None:
-                startDate = datetime.datetime.strptime('0', '%S') # EPOCH time
-            statuses = api.GetUserTimeline(screen_name=screenName, since_id=0)
-            completeStatusesText = [strTimeStamp(getTimeStamp(status.created_at)) + status.text for status in statuses
-                                    if getTimeStamp(status.created_at) < endDate and getTimeStamp(status.created_at) > startDate]
-            for singleStatusText in completeStatusesText: # TODO: add separate printing function
-                print(singleStatusText)
+        print("Getting tweets for user %s:" % screenName)
+        allTweetsList = []
+        newTweets = api.GetUserTimeline(screen_name=screenName, count=MAX_NUMBER_OF_TWEETS_PER_REQUEST)
+        allTweetsList.extend(newTweets)
+        oldestTweetId = allTweetsList[-1].id - 1
+        while len(newTweets) > 0:
+            newTweets = api.GetUserTimeline(screen_name=screenName, count=MAX_NUMBER_OF_TWEETS_PER_REQUEST,
+                                            max_id=oldestTweetId)
+            allTweetsList.extend(newTweets)
+            oldestTweetId = allTweetsList[-1].id - 1
 
-            
-            
-
+        print("Filtering tweets by date")
+        readableFormatTweetsList = [[tweet.id_str, stringTimestamp(getTimestamp(tweet.created_at)),
+                                     tweet.text.encode("utf-8")] for tweet in allTweetsList
+                                    if endDateFilter > getTimestamp(tweet.created_at) > startDateFilter]
+        saveTweetsToFile(screenName, readableFormatTweetsList)
 
 if __name__ == "__main__":
     try:
@@ -67,9 +79,7 @@ if __name__ == "__main__":
     except AssertionError as e:
         print(e.message)
         sys.exit(1)
-
-
     fetchStatusText(screenNames=config.getScreenName(),
-                    tweetsToFetch=config.getTweetsToFetch(),
-                    dates=config.getDatesToFetch())
+                    startDateFilter=config.getStartDateToFilter(),
+                    endDateFilter=config.getEndDateToFilter())
 
